ea2976659c1d89c898e5339fd5432278bb90bf49
Sean Huang <shang_huang@hotmail.com>
2021-10-01 23:24:45 +0800

    Updates

---
+++ precache-manifest.cbe559a2f644e93b94e91a955fca523f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff6f98a9383d462aebf9",
+    "url": "/ocial-network/static/css/main.b9190af6.chunk.css"
+  },
+  {
+    "revision": "ff6f98a9383d462aebf9",
+    "url": "/ocial-network/static/js/main.ff6f98a9.chunk.js"
+  },
+  {
+    "revision": "a29bcb1b0314aadbb7ff",
+    "url": "/ocial-network/static/css/1.e05fe4d3.chunk.css"
+  },
+  {
+    "revision": "a29bcb1b0314aadbb7ff",
+    "url": "/ocial-network/static/js/1.a29bcb1b.chunk.js"
+  },
+  {
+    "revision": "6e04f35f194671a25342",
+    "url": "/ocial-network/static/js/runtime~main.6e04f35f.js"
+  },
+  {
+    "revision": "573414de6757c178edfbe1c17139e908",
+    "url": "/ocial-network/index.html"
+  }
+];
